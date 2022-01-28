da296065c740b55b60d15b359eb396282e1102ca
Rohit Naiknaware <rnnaiknaware95@gmail.com>
2022-01-28 20:12:05 +0530

    Weather API fetched successfully.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+function App (){
+
+
+
+  const [city, updateCity] = useState("Pune");
+  const [weather, updateWeather] = useState();
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=abe0d13943beca2b6822fa2fc7bbae4e`;
+    const fetchWeather = async () => {
+      const response = await fetch(url)
+      const responseJSON = await response.json(); 
+      updateCity(responseJSON);
+    }
+
+    fetchWeather(); 
+  }, [])
+
+    
+
+
   return (
     <div className="App">
-      Weather App
-      <p>This is weather app.</p>
+        <p>{city.name}</p>
     </div>
   );
 }
