be29312f09c60517f416908df03918e8a7c4896d
Rohit Naiknaware <rnnaiknaware95@gmail.com>
2022-02-04 19:13:07 +0530

    TailwindCSS added successfully.

--- src/App.js
+++ src/App.js
@@ -6,26 +6,37 @@ function App (){
 
 
 
-  const [city, updateCity] = useState("Pune");
-  const [weather, updateWeather] = useState();
+  const [city, updateCity] = useState(null);
+  const [search, updateSearch] = useState("Mumbai");
 
   useEffect(() => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=abe0d13943beca2b6822fa2fc7bbae4e`;
     const fetchWeather = async () => {
       const response = await fetch(url)
       const responseJSON = await response.json(); 
-      updateCity(responseJSON);
+      updateSearch(responseJSON);
+      console.log(responseJSON.name);
     }
-
     fetchWeather(); 
-  }, [])
-
-    
+  }, [search])
 
 
   return (
     <div className="App">
-        <p>{city.name}</p>
+      <div className="input-data">
+        <input type="search" className="input-box" onChange={ (event) => {updateSearch(event.target.value)}  } />
+      </div>
+      <div className="dsplay-screen">
+        {
+          !city?(
+            <p> No data found</p>
+          ):( 
+            <div className="abc"> 
+              <p>{city.main.temp}</p>
+            </div>
+           )
+        }
+      </div>
     </div>
   );
 }
